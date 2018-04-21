6bb4857f7d64fe97d39703b25ed516f560f8311b
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-21 21:44:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,30 @@ from emoji import emojize
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
+games={}
+
 
 @bot.message_handler(commands=['startgame'])
 def startgame(m):
-    pass
+    if m.chat.id not in games:
+        games.update(creategame(m.chat.id))
+        Keyboard=types.InlineKeyboardMarkup()
+        Keyboard.add(types.InlineKeyboardButton(), text='Join', callback_data='join')
+        bot.send_message(m.chat.id, 'telegram.me/crossfirebot?start='+str(m.from_user.id), reply_markup=Keyboard)
+    else:
+        bot.send_message(m.chat.id, 'Игра уже запущена! Жмите "присоединиться"!')
     
-    
+   
+
+def creategame(id):
+    return id:{
+        'players':{}
+    }
+        
+
+
+
+
 if __name__ == '__main__':
   bot.polling(none_stop=True)
 
