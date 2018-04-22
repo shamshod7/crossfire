e1b2c734fc53a245f6543bd0c4a9de97ae965c28
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-22 15:57:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,10 @@ bot = telebot.TeleBot(token)
 games={}
 
 
+def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode='Markdown'):
+    return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
+                                 parse_mode=parse_mode)
+
 
 @bot.message_handler(commands=['start'])
 def start(m):
