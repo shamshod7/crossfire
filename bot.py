61bd1acaa0f4e4a042c3c36ad0f26caffdb97c89
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-25 13:42:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -605,6 +605,9 @@ def reallyshoot(game):
             if game['players'][ids]['killany']!=None:
                 if game['players'][ids]['killany']['role']=='prohojii':
                     win=porajenie+'Проиграл (убил прохожего)\n'
+                           
+                if game['players'][ids]['killany']['role']=='primanka':
+                    win=porajenie+'Проиграл (убил приманку)\n'
                 if game['players'][ids]['killany2']!=None:
                     if game['players'][ids]['killany2']['role']=='prohojii':
                         win=porajenie+'Проиграл (убил прохожего)\n'
