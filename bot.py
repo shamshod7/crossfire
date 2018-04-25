5a0af1af00b6f8743ec16b9b6e9a8fc9a0704ca3
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-25 16:32:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ def start(m):
                 i+=1         
             if games[int(x[1])]['play']==0:
                 games[int(x[1])]['players'].update(createuser(m.from_user.id, m.from_user.first_name, i+1))
-                text=[m.from_user.first_name](tg://user?id=m.from_user.id)
+                text=[m.from_user.first_name]('tg://user?id='m.from_user.id)
                 medit(games[int(x[1])]['userlist']+text+'\n', games[int(x[1])]['id'], games[int(x[1])]['users'])
                 games[int(x[1])]['userlist']+=text+'\n'
                 for ids in games[int(x[1])]['players']:
