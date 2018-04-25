771e57a155d021bc7db00d59a6ee8b3b213455d6
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-25 21:39:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -782,8 +782,12 @@ def createuser(id, name, x):
 
 
 
-if __name__ == '__main__':
-  bot.polling(none_stop=True)
-
+while True:
+    from requests.exceptions import ReadTimeout
+    from requests.exceptions import ConnectionError
+    try:
+        bot.polling()
+    except(ReadTimeout, ConnectionError):
+        pass
 
 
